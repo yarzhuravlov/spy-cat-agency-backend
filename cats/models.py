73b4b99ac55caf52c56045b73faac77dfe937d01
Yaroslav Zhuravlov <yaroslav.zhuravlov.dev@gmail.com>
2025-05-24 16:25:56 +0300

    add validate_name static method to Cat model which checks that name is unique

--- cats/models.py
+++ cats/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -6,3 +7,24 @@ class Cat(models.Model):
     years_of_experience = models.PositiveIntegerField()
     breed = models.CharField(max_length=63)
     salary = models.PositiveIntegerField()
+
+    @staticmethod
+    def validate_name(name: str, error_to_raise: type[Exception]):
+        if Cat.objects.filter(name__iexact=name):
+            raise error_to_raise(
+                {"name": "Cat with this Name already exists."},
+            )
+
+    def clean(self):
+        Cat.validate_name(self.name, ValidationError)
+
+    def save(
+        self,
+        *,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+        return super().save(force_insert, force_update, using, update_fields)
