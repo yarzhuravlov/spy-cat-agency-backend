4af9c5f2a869c58227f58941ac5397843c17e6ae
Yaroslav Zhuravlov <yaroslav.zhuravlov.dev@gmail.com>
2025-05-24 15:25:43 +0300

    add setup for v1 api endpoints into config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -14,13 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+api_v1_urlpatterns = []
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/", include((api_v1_urlpatterns, "v1"), namespace="v1")),
 ]
 
 if settings.DEBUG:
