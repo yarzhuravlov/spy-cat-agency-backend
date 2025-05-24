0bd1ee5e3de94f533cd08600d2a69ff0e911e979
Yaroslav Zhuravlov <yaroslav.zhuravlov.dev@gmail.com>
2025-05-24 15:42:22 +0300

    add SessionAuthentication in Debug mode and set DEFAULT_PERMISSION_CLASSES to IsAuthenticated

--- config/settings.py
+++ config/settings.py
@@ -137,3 +137,15 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
+
+if DEBUG:
+    REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"].append(
+        "rest_framework.authentication.SessionAuthentication"
+    )
